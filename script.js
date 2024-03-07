33ee61a0573b0da6c5c081fd3f37c7bd23dd2a80
AshvEmE <ashveme@gmail.com>
2024-03-07 18:28:44 +0200

    1.3

---
+++ script.js
@@ -0,0 +1,33 @@
+//типи даних
+let ynumber = 1;
+let xnumber = 2;
+let string = 'b';
+//перевірка типів
+console.log(typeof string);
+console.log(typeof ynumber);
+
+//оператори присвоєння 
+console.log(xnumber + string);
+xnumber += 10;
+console.log(xnumber);
+xnumber %= 2;
+console.log(xnumber);
+
+let boolean = true
+let logne = !boolean
+console.log(logne)
+
+
+let inull = null;
+let ibigInt = 1234567890123456789012345678901234567890n;
+let obj = {
+	name: 'kk',
+	age: 2,
+};	
+let arr = [1,2,'ll']
+let kan = NaN;
+let arr1 = [true, 2, 'mm']
+
+
+
+
